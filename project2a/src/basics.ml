6ed0cb9bddccac3f0611961911a0e23f44126869
skushnir123 <samkushnir@gmail.com>
2021-09-19 17:34:36 -0400

    Update basics.ml

--- project2a/src/basics.ml
+++ project2a/src/basics.ml
@@ -2,19 +2,24 @@
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
 
-let rev_tup tup = failwith "unimplemented"
+let rev_tup tup = match tup with
+| (a,b,c) -> (c,b,a)
 
-let is_odd x = failwith "unimplemented"
+let is_odd x = match x mod 2 with
+  | 0 -> false
+  | _ -> true
 
-let area x y = failwith "unimplemented"
+let area x y = match x,y with
+| (x1, y1), (x2, y2) -> (x2-x1) * (y2-y1)
 
-let volume x y = failwith "unimplemented"
+let volume x y = match x,y,z with
+| (x1, y1,z1), (x2, y2,z2) -> (x2-x1) * (y2-y1) * (z2-z1)
 
 (*******************************)
 (* Part 2: Recursive Functions *)
 (*******************************)
 
-let rec fibonacci n = failwith "unimplemented"
+let rec fibonacci n = 
 
 let rec pow x y = failwith "unimplemented"
 
@@ -28,9 +33,27 @@ let rec is_prime x = failwith "unimplemented"
 (* Part 3: Lists *)
 (*****************)
 
-let rec get idx lst = failwith "unimplemented"
+let rec get idx lst = match lst with
+  | [] -> failwith "Out of bounds"
+  | h::t ->
+      if idx == 0 then h
+      else get (idx-1) t
 
-let larger lst1 lst2 = failwith "unimplemented"
+
+let rec get2 idx lst = match lst with
+  | [] -> -1
+  | h::t ->
+      if idx == 0 then 1
+      else get2 (idx-1) t
+
+let rec larger_aux lst1 lst2 idx = match get2 idx lst1, get2 idx lst2 with
+  | -1, -1 -> []
+  | -1, 1 -> lst2
+  | 1, -1 -> lst1
+  | 1, 1 -> larger_aux lst1 lst2 (idx+1) 
+              
+              
+let rec larger lst1 lst2 = larger_aux lst1 lst2 0
 
 let reverse lst = failwith "unimplemented"
 
