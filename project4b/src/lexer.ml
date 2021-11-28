c250f28b668fe192540f77d17834e431aa26adc4
skushnir123 <samkushnir@gmail.com>
2021-11-28 11:39:56 -0500

    4b

--- project4b/src/lexer.ml
+++ project4b/src/lexer.ml
@@ -1,5 +1,131 @@
+
+
+
+
+
+
+
 open TokenTypes
 
-(* PASTE YOUR LEXER FROM P4A HERE *)
+(* Part 1: Lexer - IMPLEMENT YOUR CODE BELOW *)
 
-let tokenize input = failwith "unimplemented"
+let rec fold f a lst = 
+  match lst with 
+  | [] -> a
+  | h::t -> fold f (f a h) t
+
+let explode (s: string) : char list =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l)
+  in
+  exp (String.length s - 1) []
+
+let rec elem x a =
+  match a with
+  | h::t -> (h = x) || (elem x t)
+  | [] -> false
+
+let rec insert x a =
+  if not (elem x a) then x::a else a
+
+let insert_all (xs : 'a list) (a : 'a list) : 'a list =
+  List.fold_right insert xs a
+
+let rec subset a b =
+  match a with
+  | h::t -> (elem h b) && (subset t b)
+  | [] -> true
+
+let rec eq a b = (subset a b) && (subset b a)
+
+let rec reverse arr = match arr with 
+[] -> []
+| h::t -> (reverse t)@[h]
+
+let rec charArrayToString arr = match arr with
+[] -> ""
+| h::t -> 
+  let stringChar = String.make 1 h in
+  stringChar ^ (charArrayToString t)
+
+let rec tokenizeHelper readingString currentString acc inQuotes = match readingString with
+    [] -> if (eq currentString []) then acc else currentString::acc
+  | h::t -> if inQuotes = true then
+    if h = '\"' then tokenizeHelper t (currentString@[h]) acc false
+    else tokenizeHelper t (currentString@[h]) acc true
+  else if h = '\"' then tokenizeHelper t (currentString@[h]) acc true
+  else if h = ' '  then
+        if (eq currentString []) then tokenizeHelper t currentString acc inQuotes else tokenizeHelper t [] (currentString::acc) inQuotes
+      else tokenizeHelper t (currentString@[h]) acc inQuotes
+
+let tokensList str = reverse (tokenizeHelper (explode str) [] [] false)
+
+let regexList = [(Str.regexp "\\([0-9]+\\)+\\|(\\(-[0-9]+\\))",fun matchedGroup -> Tok_Int (int_of_string matchedGroup));
+                (Str.regexp "\\(fun[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(fun\\)",fun matchedGroup -> Tok_Fun);
+                (Str.regexp "\\(not[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(not\\)",fun matchedGroup -> Tok_Not);
+                (Str.regexp "\\(if[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(if\\)",fun matchedGroup -> Tok_If);
+                (Str.regexp "\\(else[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(else\\)",fun matchedGroup -> Tok_Else);
+                (Str.regexp "\\(then[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(then\\)",fun matchedGroup -> Tok_Then);
+                (Str.regexp "\\(let[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(let\\)",fun matchedGroup -> Tok_Let);
+                (Str.regexp "\\(def[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(def\\)",fun matchedGroup -> Tok_Def);
+                (Str.regexp "\\(rec[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(rec\\)",fun matchedGroup -> Tok_Rec);
+                (Str.regexp "\\(in[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(in\\)",fun matchedGroup -> Tok_In);
+                (Str.regexp "\\(true[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(true\\)",fun matchedGroup -> Tok_Bool true);
+                (Str.regexp "\\(false[a-zA-Z0-9]+\\)",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\\(false\\)",fun matchedGroup -> Tok_Bool false);
+                (Str.regexp "\\([a-zA-Z][a-zA-Z0-9]*\\)+",fun matchedGroup -> Tok_ID (matchedGroup));
+                (Str.regexp "\"\\([^\"]*\\)\"",fun matchedGroup -> Tok_String (matchedGroup));
+                (Str.regexp "\\((\\)",fun matchedGroup -> Tok_LParen);
+                (Str.regexp "\\()\\)",fun matchedGroup -> Tok_RParen);
+                (Str.regexp "\\(=\\)",fun matchedGroup -> Tok_Equal);
+                (Str.regexp "\\(<>\\)",fun matchedGroup -> Tok_NotEqual);
+                (Str.regexp "\\(>=\\)",fun matchedGroup -> Tok_GreaterEqual);
+                (Str.regexp "\\(<=\\)",fun matchedGroup -> Tok_LessEqual);
+                (Str.regexp "\\(||\\)",fun matchedGroup -> Tok_Or);
+                (Str.regexp "\\(&&\\)",fun matchedGroup -> Tok_And); 
+                (Str.regexp "\\(->\\)",fun matchedGroup -> Tok_Arrow);
+                (Str.regexp "\\(>\\)",fun matchedGroup -> Tok_Greater);
+                (Str.regexp "\\(<\\)",fun matchedGroup -> Tok_Less);
+                (Str.regexp "\\(;;\\)",fun matchedGroup -> Tok_DoubleSemi);
+                (Str.regexp "\\(+\\)",fun matchedGroup -> Tok_Add);
+                (Str.regexp "\\(-\\)",fun matchedGroup -> Tok_Sub);
+                (Str.regexp "\\(*\\)",fun matchedGroup -> Tok_Mult);
+                (Str.regexp "\\(/\\)",fun matchedGroup -> Tok_Div);
+                (Str.regexp "\\(\\^\\)",fun matchedGroup -> Tok_Concat);
+] 
+
+let rec findMatchingRegex regList str = match regList with
+| [] -> (fun matchedGroup -> Tok_Int 613)
+| (h,func)::t -> if Str.string_match h str 0 then func else findMatchingRegex t str
+
+
+let rec convertString str = 
+    let strLength = String.length str in
+    if strLength = 0 then [] else
+    let matchingRegex = (findMatchingRegex regexList str) in
+    if (matchingRegex "8") = Tok_Int 613 then raise (InvalidInputException "sorry") else
+    let indexOfEnd = (Str.match_end ()) in
+    let newLength = strLength-indexOfEnd in
+    let newSubstring = String.sub str indexOfEnd newLength in
+    let matchedGroup = 
+      try
+      (Str.matched_group 1 str) with
+      Not_found -> (Str.matched_group 2 str) 
+     in
+    (matchingRegex matchedGroup)::(convertString newSubstring)
+
+let tokenize input = 
+  let charList = tokensList input in
+  let stringList = List.map (charArrayToString) charList in
+  let tokens = (fold (fun a h -> a@(convertString h)) [] stringList) in
+  tokens
--- project4b/src/parser.ml
+++ project4b/src/parser.ml
@@ -13,7 +13,7 @@ let match_token (toks: token list) (tok: token) =
       Printf.sprintf "Expected %s from input %s, got %s"
         (string_of_token tok)
         (string_of_list string_of_token toks)
-        (string_of_token h)))
+        (string_of_token h))) 
 
 (* Matches a sequence of tokens given as the second list in the order in which they appear, throwing an error if they don't match *)
 let match_many (toks: token list) (to_match: token list) =
@@ -32,8 +32,285 @@ let rec lookahead_many (toks: token list) (n: int) =
   | _::t, n when n > 0 -> lookahead_many t (n-1)
   | _ -> None
 
-(* PASTE YOUR PARSERS FROM P4A HERE *)
+(* Part 2: Parsing expressions *)
 
-let rec parse_expr toks = failwith "unimplemented"
+let getFirst tup = match tup with
+    (a,b) -> a
 
-let rec parse_mutop toks = failwith "unimplemented"
+let getSecond tup = match tup with
+    (a,b) -> b
+
+
+let productionRecExpr toks =
+  let isRec = lookahead toks = Some Tok_Rec in
+  let newToks = if isRec then match_token toks Tok_Rec else toks in
+  (newToks, isRec)
+
+
+let productiontokIDExpr toks = 
+  let nextToken = lookahead toks in
+  match nextToken with
+    Some Tok_ID id -> ((match_token toks (Tok_ID id)), id)
+  | _ -> raise (InvalidInputException "sorryb")
+
+
+let productionXExpr toks x =
+  let nextToken = lookahead toks in
+  match nextToken with
+    Some x -> match_token toks x
+  | _ -> raise (InvalidInputException "sorryc")
+
+and isNextPrimary toks =
+  let nextToken = lookahead toks in
+  match nextToken with
+  | Some Tok_Int intVar -> 
+      true
+  | Some Tok_Bool boolVar -> 
+      true
+  | Some Tok_String stringVar -> 
+      true
+  | Some Tok_ID id -> 
+      true
+  | Some Tok_LParen -> 
+      true
+  | _ -> false 
+  
+    
+let rec productionPrimaryExpression toks =
+  let nextToken = lookahead toks in
+  match nextToken with
+  | Some Tok_Int intVar -> 
+      let toks = match_token toks (Tok_Int intVar) in
+      (toks, Value (Int intVar))
+  | Some Tok_Bool boolVar -> 
+      let toks = match_token toks (Tok_Bool boolVar) in
+      (toks, Value (Bool boolVar))
+  | Some Tok_String stringVar -> 
+      let toks = match_token toks (Tok_String stringVar) in
+      (toks, Value (String stringVar))
+  | Some Tok_ID id -> 
+      let toks = match_token toks (Tok_ID id) in
+      (toks, ID id)
+  | Some Tok_LParen -> 
+      let toks = match_token toks Tok_LParen in
+      let exprExpr1 = productionExpr toks in
+      let toks = getFirst exprExpr1 in
+      let expr1 = getSecond exprExpr1 in
+      let toks = productionXExpr toks Tok_RParen in
+      (toks, expr1)
+  | Some Tok_RParen -> raise (InvalidInputException "sorrya") 
+  | _ -> raise (InvalidInputException "sorryd") 
+and productionFunctionCallExpression toks =
+  let exprExpr1 = productionPrimaryExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let anothorPrimary = isNextPrimary toks in
+  if anothorPrimary then
+    let exprExpr2 = productionPrimaryExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    (toks, FunctionCall (expr1, expr2))
+  else
+    exprExpr1 
+and productionFunctionExpression toks =
+  let toks = productionXExpr toks Tok_Fun in
+  let tokIDExpr = productiontokIDExpr toks in
+  let toks = getFirst tokIDExpr in
+  let id = getSecond tokIDExpr in
+  let toks = productionXExpr toks Tok_Arrow in
+  let exprExpr1 = productionExpr toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  (toks, Fun (id, expr1)) 
+and productionUnaryExpression toks =
+  let nextToken = lookahead toks in
+  
+  if nextToken = Some Tok_Not then
+    let toks = productionXExpr toks Tok_Not in
+    let exprExpr1 = productionUnaryExpression toks in
+    let toks = getFirst exprExpr1 in
+    let expr1 = getSecond exprExpr1 in
+    (toks, Not expr1)
+  else
+    productionFunctionCallExpression toks 
+and productionConcatExpression toks =
+  let exprExpr1 = productionUnaryExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Concat then
+    let toks = productionXExpr toks Tok_Concat in
+    let exprExpr2 = productionConcatExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    (toks, Binop (Concat, expr1, expr2))
+  else exprExpr1 
+and productionMultiplicativeExpression toks =
+  let exprExpr1 = productionConcatExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Div || nextToken = Some Tok_Mult then
+    let toks = productionXExpr toks nextToken in
+    let exprExpr2 = productionMultiplicativeExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    if nextToken = Some Tok_Mult then
+      (toks, Binop (Mult, expr1, expr2))
+    else(toks, Binop (Div, expr1, expr2))
+  else
+    exprExpr1 
+and productionAdditiveExpression toks =
+  let exprExpr1 = productionMultiplicativeExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Add || nextToken = Some Tok_Sub then
+    let toks = productionXExpr toks nextToken in
+    let exprExpr2 = productionAdditiveExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    if nextToken = Some Tok_Add then
+      (toks, Binop (Add, expr1, expr2))
+    else (toks, Binop (Sub, expr1, expr2))
+  else
+    exprExpr1 
+and productionRelationalExpression toks =
+  let exprExpr1 = productionAdditiveExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Less || nextToken = Some Tok_Greater || nextToken = Some Tok_LessEqual || nextToken = Some Tok_GreaterEqual then
+    let toks = productionXExpr toks nextToken in
+    let exprExpr2 = productionRelationalExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    if nextToken = Some Tok_Less then
+      (toks, Binop (Less, expr1, expr2))
+    else if nextToken = Some Tok_Greater then
+      (toks, Binop (Greater, expr1, expr2))
+    else if nextToken = Some Tok_GreaterEqual then
+      (toks, Binop (GreaterEqual, expr1, expr2))
+    else
+      (toks, Binop (LessEqual, expr1, expr2)) 
+  else
+    exprExpr1 
+and productionEqualityExpression toks =
+  let exprExpr1 = productionRelationalExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Equal || nextToken = Some Tok_NotEqual then
+    let toks = productionXExpr toks nextToken in
+    let exprExpr2 = productionEqualityExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    if nextToken = Some Tok_Equal then
+      (toks, Binop (Equal, expr1, expr2))
+    else (toks, Binop (NotEqual, expr1, expr2))
+  else
+    exprExpr1 
+and productionAndExpression toks =
+  let exprExpr1 = productionEqualityExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_And then
+    let toks = productionXExpr toks nextToken in
+    let exprExpr2 = productionAndExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    (toks, Binop (And, expr1, expr2))
+  else
+    exprExpr1 
+and productionOrExpression toks =
+  let exprExpr1 = productionAndExpression toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Or then
+    let toks = productionXExpr toks nextToken in
+    let exprExpr2 = productionOrExpression toks in
+    let toks = getFirst exprExpr2 in
+    let expr2 = getSecond exprExpr2 in
+    (toks, Binop (Or, expr1, expr2))
+  else
+    exprExpr1 
+and productionLetExpr toks =
+  let toks = match_token toks Tok_Let in
+  let recursionExpr = productionRecExpr toks in
+  let toks = getFirst recursionExpr in
+  let isRec = getSecond recursionExpr in
+  let tokIDExpr = productiontokIDExpr toks in
+  let toks = getFirst tokIDExpr in
+  let id = getSecond tokIDExpr in
+  let toks = productionXExpr toks Tok_Equal in
+  let exprExpr1 = productionExpr toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let toks = productionXExpr toks Tok_In in
+  let exprExpr2 = productionExpr toks in
+  let toks = getFirst exprExpr2 in
+  let expr2 = getSecond exprExpr2 in
+  (toks, Let (id, isRec, expr1, expr2)) 
+and productionIfExpression toks =
+  let toks = match_token toks Tok_If in
+  let exprExpr1 = productionExpr toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let toks = productionXExpr toks Tok_Then in
+  let exprExpr2 = productionExpr toks in
+  let toks = getFirst exprExpr2 in
+  let expr2 = getSecond exprExpr2 in
+  let toks = productionXExpr toks Tok_Else in
+  let exprExpr3 = productionExpr toks in
+  let toks = getFirst exprExpr3 in
+  let expr3 = getSecond exprExpr3 in
+  (toks, If (expr1, expr2, expr3)) 
+and productionExpr toks = 
+  let nextToken = lookahead toks in
+  if nextToken = Some (Tok_Let) then
+    let exprExpr1 = productionLetExpr toks in
+    exprExpr1
+  else if nextToken = Some (Tok_If) then
+    let exprExpr1 = productionIfExpression toks in
+    exprExpr1
+  else if nextToken = Some (Tok_Fun) then
+    let exprExpr1 = productionFunctionExpression toks in
+    exprExpr1
+  else productionOrExpression toks
+
+
+let rec parse_expr toks = productionExpr toks
+
+(* Part 3: Parsing mutop *)
+
+let rec parse_mutop toks = 
+  let nextToken = lookahead toks in
+  if nextToken = Some Tok_Def then
+    parse_def_mutop toks
+  else if nextToken = Some Tok_DoubleSemi then
+    let toks = productionXExpr toks Tok_DoubleSemi in
+    (toks, NoOp)
+  else parse_expr_mutop toks
+
+
+and parse_def_mutop toks =
+  let toks = productionXExpr toks Tok_Def in
+  let tokIDExpr = productiontokIDExpr toks in
+  let toks = getFirst tokIDExpr in
+  let id = getSecond tokIDExpr in
+  let toks = productionXExpr toks Tok_Equal in
+  let exprExpr1 = productionExpr toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let toks = productionXExpr toks Tok_DoubleSemi in
+  (toks, Def (id, expr1))
+
+and parse_expr_mutop toks = 
+  let exprExpr1 = productionExpr toks in
+  let toks = getFirst exprExpr1 in
+  let expr1 = getSecond exprExpr1 in
+  let toks = productionXExpr toks Tok_DoubleSemi in
+  (toks, Expr expr1)
+
